96caf51f96448509c4d5d7908c15dc40dfb5875f
xzha <xzha@purdue.edu>
2014-03-17 16:27:18 -0400

    removed tail recursion

--- PA03/packing.c
+++ PA03/packing.c
@@ -18,7 +18,11 @@ int main(int argc, const char * argv[])
     int blocks = 0;
     int nodes = 0;
     tree = tree_load(argv[1], &blocks, &nodes);
-    
+    if (tree == NULL)
+      {
+	return EXIT_FAILURE;
+      }
+
     //GET ROOT
     Node root;
     root = tree_root(tree, nodes);
@@ -90,7 +94,9 @@ int tree_save(const char * filename, int blocks, Node * tree)
     fprintf(fh, "%d\n",blocks);
     for (i = 1; i <= blocks; i++)
     {
-        fprintf(fh,"%d %le %le %le %le\n", i, tree[i].width, tree[i].height, tree[i].xcoord, tree[i].ycoord);
+        fprintf(fh,"%d %le %le %le %le\n", 
+		i, tree[i].width, tree[i].height, 
+		tree[i].xcoord, tree[i].ycoord);
     }
     
     fclose(fh);
@@ -112,67 +118,67 @@ void postorder_print(Node * tree, Node * root)
 }
 
 //INORDER
+//REMOVED TAIL RECURSION
 void inorder_print(Node * tree, Node * root)
 {
-    if(root->rcnode == -1 && root->lcnode == -1)
+  while (root->rcnode != -1 && root->lcnode != -1)
     {
-        printf("%d ", root->thisnode);
-        return;
+      inorder_print(tree, &tree[root->lcnode]);
+      printf("%d ", root->thisnode);
+      root = &tree[root->rcnode];
     }
-    inorder_print(tree, &tree[root->lcnode]);
-    printf("%d ", root->thisnode);
-    inorder_print(tree, &tree[root->rcnode]);
-    return;
+  printf("%d ", root->thisnode);
+  return;
 }
 
 //PREORDER
+//REMOVED TAIL RECURSION
 void preorder_print(Node * tree, Node * root)
 {
-    
-    if(root->rcnode == -1 && root->lcnode == -1)
+  while (root->rcnode != -1 && root->lcnode != -1)
     {
-        printf("%d ", root->thisnode);
-        return;
+      printf("%d ", root->thisnode);
+      preorder_print(tree, &tree[root->lcnode]);
+      root = &tree[root->rcnode];
     }
-    printf("%d ", root->thisnode);
-    preorder_print(tree, &tree[root->lcnode]);
-    preorder_print(tree, &tree[root->rcnode]);
-    return;
+  printf("%d ", root->thisnode);
+  return;
 }
 
 //RECTANGLE COORDINATES: ASSUMING BIG RECTANGLE IS (0,0)
 //THE SMALLER RECTANGLES ARE BASED ON THE BIG RECTANGLE
+//PREORDER
+//REMOVED TAIL RECURSION
 void tree_pack_coordinate(Node * tree, Node * root)
 {
-    if(root->rcnode == -1 && root->lcnode == -1)
+  while(root->rcnode != -1 && root->lcnode != -1)
     {
-        return;
+      if (root -> cutline == 'V')
+	{
+	  //LC IS TO THE LEFT
+	  tree[root->lcnode].xcoord = root->xcoord;
+	  tree[root->lcnode].ycoord = root->ycoord;
+	  //RC IS TO THE RIGHT
+	  tree[root->rcnode].xcoord = root->xcoord + tree[root->lcnode].width; //ADD WIDTH OF LC
+	  tree[root->rcnode].ycoord = root->ycoord;
+	}
+      else if (root-> cutline == 'H')
+	{
+	  //LC IS ABOVE
+	  tree[root->lcnode].xcoord = root->xcoord;
+	  tree[root->lcnode].ycoord = root->ycoord + tree[root->rcnode].height; //ADD HEIGHT OF RC
+	  //RC IS BELOW
+	  tree[root->rcnode].xcoord = root->xcoord;
+	  tree[root->rcnode].ycoord = root->ycoord;
+	}
+      tree_pack_coordinate(tree, &tree[root->lcnode]);
+      root = &tree[root->rcnode];
     }
-    
-    if (root -> cutline == 'V')
-    {
-        //LC IS TO THE LEFT
-        tree[root->lcnode].xcoord = root->xcoord;
-        tree[root->lcnode].ycoord = root->ycoord;
-        //RC IS TO THE RIGHT
-        tree[root->rcnode].xcoord = root->xcoord + tree[root->lcnode].width; //ADD WIDTH OF LC
-        tree[root->rcnode].ycoord = root->ycoord;
-    }
-    else if (root-> cutline == 'H')
-    {
-        //LC IS ABOVE
-        tree[root->lcnode].xcoord = root->xcoord;
-        tree[root->lcnode].ycoord = root->ycoord + tree[root->rcnode].height; //ADD HEIGHT OF RC
-        //RC IS BELOW
-        tree[root->rcnode].xcoord = root->xcoord;
-        tree[root->rcnode].ycoord = root->ycoord;
-    }
-    tree_pack_coordinate(tree, &tree[root->lcnode]);
-    tree_pack_coordinate(tree, &tree[root->rcnode]);
-    return;
+  return;
 }
 
 //CONSTRUCT RECTANGLE
+//POSTORDER
 void tree_pack_rectangle(Node * tree, Node * root)
 {
     if(root->rcnode == -1 && root->lcnode == -1)
@@ -272,8 +278,10 @@ Node * tree_load(const char * filename, int * blocks, int * nodes)
     //IMPORTANT: ONE BASED ARRAY OF NODES
     while (!feof(fh) && i < (*blocks) + 1)
     {
-        g = fscanf(fh,"%d %d %d %d %c %lf %lf", &tree[i].thisnode, &tree[i].parnode, &tree[i].lcnode, &tree[i].rcnode, &tree[i].cutline,
-               &tree[i].width, &tree[i].height);
+        g = fscanf(fh,"%d %d %d %d %c %lf %lf", 
+		   &tree[i].thisnode, &tree[i].parnode, &tree[i].lcnode, 
+		   &tree[i].rcnode, &tree[i].cutline, &tree[i].width, 
+		   &tree[i].height);
         i++;
     }
 
@@ -287,7 +295,9 @@ Node * tree_load(const char * filename, int * blocks, int * nodes)
 
     while (!feof(fh) && i < (*nodes) + 1)
     {
-        g = fscanf(fh,"%d %d %d %d %c %*c %*c", &tree[i].thisnode, &tree[i].parnode, &tree[i].lcnode, &tree[i].rcnode, &tree[i].cutline);
+        g = fscanf(fh,"%d %d %d %d %c %*c %*c", 
+		   &tree[i].thisnode, &tree[i].parnode, &tree[i].lcnode, 
+		   &tree[i].rcnode, &tree[i].cutline);
         tree[i].width = 0;
         tree[i].height = 0;
         i++;
